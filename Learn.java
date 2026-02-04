2f9eaba3fde23787602da7ff81cd62c382853b90
amymariya1 <amymariya2026@mca.ajce.in>
2026-02-04 19:26:32 +0530

    Add files via upload

---
+++ Learn.java
@@ -0,0 +1,12 @@
+public class Learn{
+	public static void main(String[] args){
+		int[] arr = new int[5];
+		arr[0] = 23;
+		arr[1] = 21;
+		arr[2] = 56;
+		arr[3] =899;
+		arr[4] =563;
+		
+		System.out.println(arr[2]);
+	}
+}
